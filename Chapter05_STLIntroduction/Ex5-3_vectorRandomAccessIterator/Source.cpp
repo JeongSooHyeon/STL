4de5d8b5f61fee0d38eaab6e436a6a67426a9ecf
SooHyeon Jeong <85021523+Been98@users.noreply.github.com>
2023-05-03 20:52:11 +0900

    Chapter05 STL소개

---
+++ Chapter05_STLIntroduction/Ex5-3_vectorRandomAccessIterator/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+
+    vector<int>::iterator iter = v.begin();
+    cout << iter[0] << endl;    // [] 연산자
+    cout << iter[1] << endl;
+    cout << iter[2] << endl;
+    cout << endl;
+
+    iter += 2;  // += 연산
+    cout << *iter << endl;
+    cout << endl;
+
+    vector<int>::iterator iter2 = iter + 2; // + 연산
+    cout << *iter2 << endl;
+
+    return 0;
+}
---
+++ Chapter05_STLIntroduction/Ex5-9_vector_stack/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int, vector<int>> st;  // vector 컨테이너를 이용한 stack 컨테이너 생성
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << st.top() << endl;   // top 데이터 출력
+    st.pop();                   // top 데이터 삭제
+
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    if (st.empty())  // stack이 비었는지 확인
+        cout << "stack에 데이터 없음" << endl;
+
+    return 0;
+}
